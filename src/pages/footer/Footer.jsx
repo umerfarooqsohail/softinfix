802d85d1ee8d131be41974d02671dececf091170
umer farooq soahil <malikumerfarooqsoahil@gmail.com>
2021-04-08 01:02:57 +0500

    navbar

---
+++ src/pages/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-light w-100 text-center ">
+        <footer>
+          <p>© 2020 UmerTechnical.All Right Reserved | Terms and Condition</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+export default Footer;
